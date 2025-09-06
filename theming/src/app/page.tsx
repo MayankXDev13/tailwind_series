f70dc57021c8fc349961afc537743452e90a1e49
Mayank <vijaymaynk2003@gmail.com>
2025-09-06 23:22:56 +0530

    tailwind theme

---
+++ theming/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center bg-neutral-900">
+      <Hero />
+    </div>
+  );
+}
