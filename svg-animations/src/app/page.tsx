16e0505d730ca8463b72bc414a8e283515fb0512
Mayank <vijaymaynk2003@gmail.com>
2025-09-19 10:36:50 +0530

    svg animation

---
+++ svg-animations/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { SvgLines } from "@/components/SvgLines";
+
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-gray-50 max-w-4xl mx-auto flex items-center flex-col py-20">
+      <SvgLines/>
+    </main>
+  );
+}
+
+
+
+
